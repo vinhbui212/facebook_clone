f6c0eddfa4595b6bef85f481cc4d5b3d2289658a
vinhbui212 <100837872+vinhbui212@users.noreply.github.com>
2023-03-14 12:43:28 +0700

    Update ImageUpload.js

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -5,7 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import Button from "@material-ui/core/Button";
 import { storage, db } from "./firebase.js";
-import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import './ImageUpload.css';
 import firebase from "firebase";
 
